68d162cf10c127b945981f603e3a4e909b57e4e4
WizzC <clementcarlier310@gmail.com>
2024-01-15 10:20:59 +0100

    infosFinit

--- front/script.js
+++ front/script.js
@@ -7,12 +7,19 @@ const liste = document.querySelector('.listeSalle');
 const barreRecherche = document.querySelector('.barreRecherche');
 const filtreForm = document.querySelector('#filtreForm');
 let tabStyle = [];
+let listeSalles;
+let map = L.map('map').setView([46, 2], 5);
+let markersLayer = L.layerGroup().addTo(map);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 20,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 // Écouteurs d'événements
 filtreForm.addEventListener('change', filterSalles);
 barreRecherche.addEventListener('input', filterSalles);
 btnFiltre.addEventListener('click', fermetureStyleFlitre);
-
 // Connexion à l'API pour charger les différents styles de musique
 fetch('https://localhost:44314/api/Styles')
     .then(res => res.json())
@@ -30,6 +37,16 @@ fetch('https://localhost:44314/api/Salles')
     .then(salles => {
         // Créer les éléments de salle à partir des données récupérées
         creerElementSalles(salles);
+        createAllPointsInMap(salles);
+        listeSalles = document.querySelectorAll('.salle');
+        for (let i = 0; i < listeSalles.length; i++) {
+
+            listeSalles[i].addEventListener('click', (e) => {
+                localStorage.setItem('id', listeSalles[i].querySelector('#idSalle').textContent)
+                document.location.href = "http://127.0.0.1:5500/salle.html";
+            });
+        }
+
     })
     .catch(error => console.error('Erreur de fetch pour les salles :', error));
 
@@ -40,7 +57,17 @@ function filterSalles() {
         .then(res => res.json())
         .then(salles => {
             // Appliquer le filtrage sur les salles
-            filtre(salles);
+            let test = filtre(salles);
+            effacerMarqueurs();
+            createAllPointsInMap(test);
+            listeSalles = document.querySelectorAll('.salle');
+            for (let i = 0; i < listeSalles.length; i++) {
+
+                listeSalles[i].addEventListener('click', (e) => {
+                    localStorage.setItem('id', listeSalles[i].querySelector('#idSalle').textContent)
+                    document.location.href = "http://127.0.0.1:5500/salle.html";
+                });
+            }
         })
         .catch(error => console.error('Erreur de fetch pour les salles :', error));
 }
@@ -51,7 +78,7 @@ function filtre(salles) {
 
     const filteredResults = salles.filter(
         item => (item.nom.toLowerCase().includes(filtreNom) || item.adresse.ville.toLowerCase().includes(filtreNom) || String(item.capacite).includes(filtreNom)) &&
-        tabStyle.every(style => item.styles.includes(style))
+            tabStyle.every(style => item.styles.includes(style))
     );
 
     // Suppression des éléments existants dans la liste
@@ -62,6 +89,7 @@ function filtre(salles) {
 
     // Création et ajout des nouveaux éléments
     creerElementSalles(filteredResults);
+    return filteredResults;
 }
 
 // Fonction de création et ajout des éléments au DOM à partir des données des salles
@@ -71,12 +99,12 @@ function creerElementSalles(salles) {
         var nomSalle = clone.querySelector('h3');
         var villeSalle = clone.querySelector('#ville');
         var capaciteSalle = clone.querySelector('#capacite');
-
+        var idSalle = clone.querySelector('#idSalle');
         // Mise à jour des données de la salle dans le template
         nomSalle.textContent = salle.nom;
         villeSalle.textContent = salle.adresse.ville;
         capaciteSalle.textContent = salle.capacite;
-
+        idSalle.textContent = salle.id;
         // Ajout du clone à la liste
         liste.appendChild(clone);
     });
@@ -121,5 +149,19 @@ function fermetureStyleFlitre() {
     } else {
         filtreOpen.style.display = '';
         btnFiltre.textContent = '<';
+    }
+}
+
+
+function createAllPointsInMap(salles) {
+    salles.forEach(salle => {
+        let salleCoordinates = [salle.adresse.localisation.coordinates[0], salle.adresse.localisation.coordinates[1]];
+        L.marker(salleCoordinates).addTo(markersLayer);
+    });
+}
+
+function effacerMarqueurs() {
+    if (markersLayer) {
+        markersLayer.clearLayers();
     }
 }
